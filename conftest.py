b3e505770c005034a4de0f6af713361cd78ccbe5
AnnaSam6 <anna.vitaeva@inbox.ru>
2026-02-19 22:05:02 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,12 +3,10 @@ from model_bakery import baker
 from rest_framework.test import APIClient
 from students.models import Course, Student
 
-
 @pytest.fixture
 def api_client():
     """Фикстура для API клиента"""
     return APIClient()
-
 
 @pytest.fixture
 def course_factory():
@@ -16,7 +14,6 @@ def course_factory():
     def factory(**kwargs):
         return baker.make(Course, **kwargs)
     return factory
-
 
 @pytest.fixture
 def student_factory():
